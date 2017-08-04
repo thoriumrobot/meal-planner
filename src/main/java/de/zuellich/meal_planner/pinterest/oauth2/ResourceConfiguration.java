825f77beb31dbbe979e88b85fd4671ff74c098f6
fzuellich <fzuellich@gmail.com>
2017-08-04 13:41:02 +0200

    Add profiles to DI container for testing on Travis CI.
    When runnning unit tests on Travis CI no proper application property configuration exists so the ApplicationContext can't be build. Adding a bean configuration that does not rely on the existence of such a configuration fixes the problem.

--- src/main/java/de/zuellich/meal_planner/pinterest/oauth2/ResourceConfiguration.java
+++ src/main/java/de/zuellich/meal_planner/pinterest/oauth2/ResourceConfiguration.java
@@ -3,6 +3,7 @@ package de.zuellich.meal_planner.pinterest.oauth2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
@@ -54,8 +55,30 @@ public class ResourceConfiguration {
      * @param clientContext The context that stores information for the oauth2 requests.
      * @return A new rest template that can be used to make requests to protected resources.
      */
-    @Bean
+    @Bean("pinterestRestTemplate")
+    @Profile("default")
     public OAuth2RestTemplate pinterestRestTemplate(OAuth2ClientContext clientContext) {
+        return new OAuth2RestTemplate(pinterestOAuth2Configuration(), clientContext);
+    }
+
+    /**
+     * Get a new rest template with OAuth2 support for Pinterest.
+     * @param clientContext The context that stores information for the oauth2 requests.
+     * @return A new rest template that can be used to make requests to protected resources.
+     */
+    @Bean("pinterestRestTemplate")
+    @Profile("testing")
+    public OAuth2RestTemplate staticallyConfiguredRestTemplate(OAuth2ClientContext clientContext) {
+        AuthorizationCodeResourceDetails resourceDetails = new AuthorizationCodeResourceDetails();
+        resourceDetails.setClientId("clientId");
+        resourceDetails.setClientSecret("clientSecret");
+        resourceDetails.setAccessTokenUri("accessTokenUri");
+        resourceDetails.setUserAuthorizationUri("authorizationUri");
+        resourceDetails.setClientAuthenticationScheme(AuthenticationScheme.query);
+        resourceDetails.setScope(Collections.singletonList("read_public"));
+        resourceDetails.setId("pinterest");
+        resourceDetails.setAuthenticationScheme(AuthenticationScheme.query);
+
         return new OAuth2RestTemplate(pinterestOAuth2Configuration(), clientContext);
     }
 
